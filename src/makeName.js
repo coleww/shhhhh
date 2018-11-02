abb5af85dfc6f77cfc386689c78394aae0a2d610
Cole Willsea <coleww@gmail.com>
2018-11-01 17:27:01 -0700

    use glitch wordset for names cuz its more better

--- src/makeName.js
+++ src/makeName.js
@@ -1,25 +1,9 @@
-var markov = require('fast-ish-markov')
+var fw = require('friendly-words');
 
-// TODO: probably do something better
-var words = []
-  .concat(require('./textData/animals'))
-  .concat(require('./textData/flowers'))
-  .concat(require('./textData/instruments'))
-  .concat(require('./textData/planets'))
-  .map(function (word) {
-    return word.split('').join(' ').replace(/\s\s\s/, ' ')
-  })
-
-function makeBigNumber() {
-  var numbers = [2, 4, 5, 888, 1000, 2000, 5000, 10000, 100000, 100000000, 101, 202, 303, 404, 505, 606, 707, 808, 909]
-  return numbers[~~(Math.random() * numbers.length)]
-}
+var enders = fw.collections.concat(fw.teams).concat(fw.objects)
 
 module.exports = function () {
-  var ngram = 3 + ~~(Math.random() * 3)
-  var m = markov(words, ngram)
-  var length = 2 + ~~(Math.random() * 10)
-  var name = m.fill(m.pick(), 15).replace(/\s/g, '')
-  if (Math.random() < 0.5) name += makeBigNumber()
-  return name
+  var start = fw.predicates[~~(Math.random() * fw.predicates.length)]
+  var end = enders[~~(Math.random() * fw.predicates.length)]
+  return `${start}-${end}`;
 }
